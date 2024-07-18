95de588da4bee2fa2129e44c468dfb9f9660b93a
Balu vusa <mr.balumca@gmail.com>
2024-07-18 22:27:54 +0530

    modified the file

--- variables.sh
+++ variables.sh
@@ -1,10 +1,11 @@
 ##Extention FoxUnderMoon
+
 ## Ctrl+ Shift + P --> Open user settings --> Text Editor --> Formatting --> FormatOnSave (Option)
 # This script  provides list of VPC-ID from the selected region.
 #!/bin/bash
 VPCID=$(aws ec2 describe-vpcs --region us-east-1 | jq ".Vpcs[].VpcId" -r)
 for vpc in $VPCID; do
     echo "--------------------------"
-    echo "The VPC ID is: $VPC"
+    echo "The VPC ID is: $vpc"
     echo "--------------------------"
 done
